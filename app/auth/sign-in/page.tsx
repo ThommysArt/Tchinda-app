35d3add26405fff32990c92d3542da420bdac770
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2024-10-27 13:36:12 +0100

    added suspense to sign-in form

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,9 +1,10 @@
+import { Suspense } from 'react'
 import SignInForm from '../_components/sign-in-form'
 
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gradient-to-b from-primary/20 to-background">
-      <SignInForm />
+      <Suspense><SignInForm /></Suspense>
     </div>
   )
 }
